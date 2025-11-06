40a20561c1f1717f6a77f498ab48b15e95cbdb33
ismooz <69241713+ismooz@users.noreply.github.com>
2025-11-06 08:01:07 +0100

    Remove onboarding remnants incompatible with Odoo 19

--- dms/models/__init__.py
+++ dms/models/__init__.py
@@ -11,12 +11,9 @@ from . import storage
 from . import directory
 from . import dms_file
 
-from . import onboarding_onboarding
-from . import onboarding_onboarding_step
 from . import dms_category
 from . import tag
 
-from . import res_company
 from . import res_config_settings
 from . import ir_attachment
 from . import ir_binary
